0ab510d0bd06f59b96c6bfd964ee0b46d79dd479
0xkenn <kennycharlest@gmail.com>
2024-06-09 21:03:10 +0800

    fixed overflow

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import AboutPage from "./Pages/AboutPage"
+import Footer from "./layout/Footer"
+import NavBar from "./layout/NavBar"
 
 
 
 const App = () => {
   return (
    <>
-     <div className="h-screen"><AboutPage/></div>
+  
+     <div className="h-screen">
+     <NavBar/>
+      <AboutPage/>
+      <Footer/></div>
+      
    </>
   )
 }
--- src/Pages/AboutPage.jsx
+++ src/Pages/AboutPage.jsx
@@ -1,13 +1,13 @@
 import About from "../layout/About"
-import Footer from "../layout/Footer"
-import NavBar from "../layout/NavBar"
+
+
 
 const AboutPage = () => {
   return (
     <>
-        <NavBar/>
+        
         <About/>
-        <Footer/>
+        
     </>
   )
 }
